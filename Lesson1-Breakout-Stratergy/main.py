e74a024b07d93bfd93a25641f381e18d097a90b3
samukelolukhele <samukelo.business@gmail.com>
2022-12-09 11:12:22 +0200

    first commit

---
+++ Lesson1-Breakout-Stratergy/main.py
@@ -0,0 +1,80 @@
+# region imports
+from AlgorithmImports import *
+# endregion
+
+
+# Plug this code into quantconnect.com to run and see the results
+import numpy as np
+
+class CalculatingYellowGreenAligator(QCAlgorithm):
+    def Initialize(self):
+        #Amount available to be traded. 
+        self.SetCash(100000)
+
+        #Trading period
+        self.SetStartDate(2019, 12,1)
+        self.SetEndDate(2022, 12, 1)
+
+        #Stock to be traded
+        self.symbol = self.AddEquity('SPY', Resolution.Daily).Symbol
+
+        #Determines the breakout amount but this value is going to
+        #be dynamically changed.
+        self.lookback = 20
+
+        #Sets upper and lower bounds for the lookback
+        self.ceiling, self.floor = 30, 10
+
+        #Initial stop loss
+        self.initialStopRisk = 0.98
+
+        #Trailing stop loss
+        self.trailingStopRisk = 0.9
+
+        #
+        self.Schedule.On(self.DateRules.EveryDay(self.symbol), \
+            self.TimeRules.AfterMarketOpen(self.symbol, 20), \
+                Action(self.EveryMarketOpen))
+
+    def onData(self, data):
+        #Capturing data to plot on the data chart.
+        self.Plot('Data Chart', self.symbol, self.Securities[self.symbol].Close)
+
+    def EveryMarketOpen(self):
+        close = self.History(self.symbol, 31, Resolution.Daily)['close']
+        todayvol = np.std(close[1:31])
+        yesterdayvol = np.std(close[0: 30])
+        deltavol = (todayvol - yesterdayvol) / todayvol
+        self.lookback = round(self.lookback * (1+ deltavol))
+
+        if self.lookback > self.ceiling: 
+            self.lookback = self.ceiling
+        elif self.lookback < self.floor: 
+            self.lookback = self.floor
+        
+        self.high = self.History(self.symbol, self.lookback, Resolution.Daily)['high']
+
+        if not self.Securities[self.symbol].Invested and \
+            self.Securities[self.symbol].Close >= max(self.high[:-1]):
+            self.SetHoldings(self.symbol, 1)
+            self.breakoutlvl = max(self.high[:-1])
+            self.highestPrice = self.breakoutlvl
+
+        if self.Securities[self.symbol].Invested:
+            if not self.Transactions.GetOpenOrders(self.symbol):
+                self.stopMarketTicket = self.StopMarketOrder(self.symbol, \
+                    -self.Portfolio[self.symbol].Quantity, \
+                        self.initialStopRisk * self.breakoutlvl)
+
+            if self.Securities[self.symbol].Close > self.highestPrice and \
+                self.initialStopRisk * self.breakoutlvl < self.Securities[self.symbol].Close * self.trailingStopRisk: 
+                self.highestPrice = self.Securities[self.symbol].Close
+                updateFields = UpdateOrderFields()
+                updateFields.stopPrice = self.Securities[self.symbol].Close * self.trailingStopRisk
+                self.stopMarketTicket.Update(updateFields);
+
+                self.Debug(updateFields.stopPrice)
+            self.Plot('Data Chart', 'Stop Price', self.stopMarketTicket.Get(OrderField.StopPrice)) 
+
+
+    
